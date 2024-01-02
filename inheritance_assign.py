35c8f247d4ab54fce69ca6325045afd124bb4b90
Brandon-J <johnson.c.brandon@gmail.com>
2024-01-02 10:05:41 -0500

    Initial commit

---
+++ inheritance_assign.py
@@ -0,0 +1,19 @@
+
+class Nissan:
+    def __init__(self, wheels, doors, model, fuel_type,passengers)
+        self.wheels = 4
+        self.doors = 0
+        self.model = ' '
+        self.fuel_type = ' '
+        self.passengers = ' '
+
+class Altima(Nissan):
+    def __init__(self, wheels, doors, model, fuel_type, passengers)
+        self.transmission = ' '
+        self.airbags = ' '
+
+class Rogue(Nissan):
+    def __init__(self, wheels, doors, model, fuel_type, passengers)
+        self.cargo_space = ' '
+        self.roof_rails = ' '
+    

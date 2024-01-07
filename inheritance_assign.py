e7b17c89b302e2f03ede6a101130cc7ec352dd8b
Brandon-J <johnson.c.brandon@gmail.com>
2024-01-07 17:59:32 -0500

    modified inheritance_assign.py to properly to initialize the child classes

--- inheritance_assign.py
+++ inheritance_assign.py
@@ -1,19 +1,33 @@
-
+# Parent class with attributes/properties
 class Nissan:
-    def __init__(self, wheels, doors, model, fuel_type,passengers)
-        self.wheels = 4
-        self.doors = 0
-        self.model = ' '
-        self.fuel_type = ' '
-        self.passengers = ' '
+    # def __init__(self, wheels, doors, model, fuel_type,passengers)
+        wheels = 0
+        doors = 0
+        model = ' '
+        fuel_type = ' '
+        passengers = 0
 
+        # Utilizing the dunder method to attach the arguments from the parent object to the attributes to the object
+        def __init__(self, wheels, doors, model, fuel_type, passengers):
+                self.wheels = wheels
+                self.doors = doors
+                self.model = model
+                self.fuel_type = fuel_type
+                self.passengers = passengers
+
+# Child class
 class Altima(Nissan):
-    def __init__(self, wheels, doors, model, fuel_type, passengers)
-        self.transmission = ' '
-        self.airbags = ' '
+    transmission = "Automatic"
+    airbags = 5
 
+# Second child class 
 class Rogue(Nissan):
-    def __init__(self, wheels, doors, model, fuel_type, passengers)
-        self.cargo_space = ' '
-        self.roof_rails = ' '
+        cargo_space = "36.3 to 36.5 ft"
+        roof_rails = True
+
+
+
+if __name__ == "__main__":
+   New_Altima = Altima(4,4,"Altima","gasoline",5)
+   print(New_Altima.transmission)
     
